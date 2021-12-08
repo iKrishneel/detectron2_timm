5416fd2b1f2223f6a93a2789827c47c96e847f13
iKrishneel <krishneel.chand.chaudhary@gmail.com>
2021-12-08 21:19:21 +0900

    :sparkles: added crossformer model

--- detectron2_timm/models/__init__.py
+++ detectron2_timm/models/__init__.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 
-from .models import (  # NOQA: F401
+from typing import List
+
+from .build import (  # NOQA: F401
     build_detectron2_backbone,
     build_detectron2_fpn_backbone,
 )
+
+from .cross_former import cross_former_s, cross_former_b  # NOQA: F401
+
+
+def list_models() -> List[str]:
+    return [
+        'cross_former_s',
+        'cross_former_b'
+    ]
